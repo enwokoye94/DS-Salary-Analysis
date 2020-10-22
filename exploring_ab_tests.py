da12a9e4fd7094f3a300fadc80a307272dc2ecec
Ekene Nwokoye <enwokoye94@hotmail.com>
2020-10-21 21:28:11 -0500

    chloropleth maps

--- exploring_ab_tests.py
+++ exploring_ab_tests.py
@@ -1,9 +1,34 @@
-import pandas as pd
-import numpy as np
-# import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
+def choropleth_map(df, agg, locations, color_scale):
+	fig = go.Figure(data=go.Choropleth(locations=df[locations],
+		z = df[agg].astype(float),
+		locationmode= 'USA-states',
+		colorscale = color_scale,
+		autocolorscale=False,
+    # text = df[agg],
+    	marker_line_color = 'white'
 
-df = pd.read_csv('cleaned_list.csv')
-print(df.columns)
+                                      ))
+	fig.update_layout(
+    	title_text='Avg. {} Per State'.format(agg),
+        geo = dict(
+        	scope='usa',
+            projection=go.layout.geo.Projection(type = 'albers usa'),
+            showlakes=True, # lakes
+            lakecolor='rgb(255, 255, 255)'),
+    )
 
-print(df.nuique())
+	app = dash.Dash()
+	app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+	app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
+
+	fig.show()
+
+choropleth_map(df_state, 'avg_salary', 'state', 'blues' )
